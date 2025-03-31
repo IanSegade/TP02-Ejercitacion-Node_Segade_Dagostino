1c91bf8540be1c9770ff4a0583ce107bb981cc04
Android Studio <devandroid@ort>
2025-03-31 12:02:03 -0300

    tp terminado

--- src/ej04.js
+++ src/ej04.js
@@ -3,8 +3,13 @@ const ARCHIVO_ENTRADA = "./archivo-entrada.txt";
 const ARCHIVO_SALIDA = "./archivo-salida.txt";
 console.clear();
 copiar(ARCHIVO_ENTRADA, ARCHIVO_SALIDA);
-function copiar(origen, destino){
-    import { readFile } from 'node:fs';
-
-    readFile('/etc/passwd', 'utf8', callback); 
+import { copyFile, constants } from 'node:fs/promises';
+async function copiar(origen, destino) {
+    try {
+        await copyFile(origen, destino);
+        console.log(origen + ' sus contenidos fueron copiados a' + destino);
+    } 
+    catch {
+        console.error('El archivo no pudo ser copiado');
+    }
 }
---
+++ src/ej08.js
@@ -0,0 +1,5 @@
+import {OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID} from './modules/omdb-wrapper.js';
+
+console.log(OMDBSearchByPage());
+console.log(OMDBSearchComplete());
+console.log(OMDBGetByImdbID());
